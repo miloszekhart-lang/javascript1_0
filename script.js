5b4e5ca15b71767e47bf01eb181e30d96534279a
miloszekhart-lang <miloszek.hart@gmail.com>
2025-10-07 09:23:34 +0000

    C 1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-(function () {
-  //TODO
-})();
+function wypiszLiczby() {
+  let str = "";
+
+  for (let i = 0; i < 10; i++) {
+    str += i;
+    if (i < 9) {
+      str += ', ';
+    }
+  }
+  document.getElementById("liczby").innerHTML = str;
+};
