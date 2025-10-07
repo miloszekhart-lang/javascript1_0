ef67bd0530487b92a05e1164008efe0d1321fa8d
miloszekhart-lang <miloszek.hart@gmail.com>
2025-10-07 09:52:30 +0000

    C 1_1.2

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function wypiszLiczby() {
   }
   document.getElementById("ex1_content").innerHTML = str;
 };
+
+function validatePhoneNumber() {
+  let number = document.getElementById("ex2_text").value;
+  var regExp = /[a-zA-Z]/g;
+  var specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (number.length != 9) {
+    document.getElementById("ex2_content").innerHTML = "Długość numeru musi być równa 9";
+  }
+  else if (regExp.test(number)) {
+    document.getElementById("ex2_content").innerHTML = "Numer nie może zawierać liter";
+  }
+  else if (specialChar.test(number)) {
+    document.getElementById("ex2_content").innerHTML = "Numer nie może zawierać znaków specjalnych";
+  }
+  else {
+    document.getElementById("ex2_content").innerHTML = "Numer telefonu jest poprawny";
+  }
+};
